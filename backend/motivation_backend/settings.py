01a5af7fb46f09ef8cde98715be82a01526c0d5e
yonassolomon <yonaisol21@gmail.com>
2026-02-27 15:28:21 -1000

    create the model part and modefing the setting inorder to frontend and backend to exchange datas

--- backend/motivation_backend/settings.py
+++ backend/motivation_backend/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'quotes',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -115,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
--- backend/quotes/models.py
+++ backend/quotes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Quote(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    text=models.CharField(max_length=300)
+    author=models.models.CharField(max_length=150,blank=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.user.username}:{self.text[:30]}"
